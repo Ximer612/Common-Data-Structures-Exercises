0d884b8ae0f4e5692a68a51204829b1b0bd56b2c
Ximer <20438890+Ximer612@users.noreply.github.com>
2023-12-02 17:51:41 +0100

    Some fix to linked list and started doubly linked list

--- LinkedList/linkedlist.c
+++ LinkedList/linkedlist.c
@@ -1,38 +1,37 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-#define MY_LIST(x) &(x.list_item)
-#define TO_GENERIC_LIST(x) (struct my_list_item*)(x)
-#define TO_INT_LIST(x) (struct my_int_item*)(x)
+#define GET_MY_LIST(x) &(x.list_item)
+#define TO_GENERIC_LIST(x) (struct list_item*)(x)
+#define TO_INT_LIST(x) (struct list_int_item*)(x)
 
 #define SET_BLUE_PRINT(x) printf("\033[0;34m")
 #define SET_GREEN_PRINT(x) printf("\033[0;32m")
 #define SET_RED_PRINT(x) printf("\033[0;31m")
 #define SET_DEFAULT_PRINT(x) printf("\033[0;00m")
 
-struct my_list_item
+struct list_item
 {
-    struct my_list_item* next; 
+    struct list_item* next; 
     unsigned int count;   
 };
 
-struct my_int_item
+struct list_int_item
 {
-    struct my_list_item list_item; //it's like his parent
+    struct list_item list_item; //it's like his parent
     int value;    
 };
 
-struct my_list_item* my_list_get_tail(struct my_list_item* head)
+struct list_item* list_get_tail(struct list_item* head)
 {
     if(!head)
     {
         return NULL;
     }
 
-    struct my_list_item* current_item = head;
-    struct my_list_item* last_item = NULL;
+    struct list_item* current_item = head;
+    struct list_item* last_item = NULL;
     
     while(current_item)
     {
@@ -43,9 +42,9 @@ struct my_list_item* my_list_get_tail(struct my_list_item* head)
     return last_item;
 }
 
-struct my_list_item* my_list_append(struct my_list_item** head, struct my_list_item* item)
+struct list_item* list_append(struct list_item** head, struct list_item* item)
 {
-    struct my_list_item* tail = my_list_get_tail(*head);
+    struct list_item* tail = list_get_tail(*head);
 
     if(!(tail))
     {
@@ -61,14 +60,14 @@ struct my_list_item* my_list_append(struct my_list_item** head, struct my_list_i
     return item;
 }
 
-struct my_list_item* my_list_pop(struct my_list_item** head)
+struct list_item* list_pop(struct list_item** head)
 {
     if(!(*head))
     {
         return NULL;
     }
 
-    struct my_list_item* current_head = *head;
+    struct list_item* current_head = *head;
     const unsigned int current_count = (*head)->count;
     *head = (*head)->next;
     if(*head)
@@ -80,16 +79,16 @@ struct my_list_item* my_list_pop(struct my_list_item** head)
     return current_head;
 }
 
-unsigned int my_list_length(struct my_list_item* head)
+unsigned int list_length(struct list_item* head)
 {
     return head->count;
 }
 
-unsigned int my_list_length_slow(struct my_list_item* head)
+unsigned int list_length_slow(struct list_item* head)
 {
     unsigned int counter = 0;
 
-    struct my_list_item* current_item = head;
+    struct list_item* current_item = head;
 
     while(current_item) // WHILE IS NOT NULL
     {
@@ -100,7 +99,7 @@ unsigned int my_list_length_slow(struct my_list_item* head)
     return counter;
 }
 
-struct my_list_item* my_list_remove_item(struct my_list_item** head, unsigned int index)
+struct list_item* list_remove_item_at_index(struct list_item** head, unsigned int index)
 {
     if(!(*head) || index > (*head)->count) return NULL;
 
@@ -109,11 +108,11 @@ struct my_list_item* my_list_remove_item(struct my_list_item** head, unsigned in
         SET_RED_PRINT();
         printf("#Removed the head element");
         SET_DEFAULT_PRINT();
-        return my_list_pop(head);
+        return list_pop(head);
     }
 
-    struct my_list_item* current_item = (*head)->next;
-    struct my_list_item* previous_item = (*head);
+    struct list_item* current_item = (*head)->next;
+    struct list_item* previous_item = (*head);
 
     for (int i = 1; i < (*head)->count; i++)
     {
@@ -124,7 +123,7 @@ struct my_list_item* my_list_remove_item(struct my_list_item** head, unsigned in
             current_item->next=NULL;
 
             SET_RED_PRINT();
-            printf("#Removed the %d element\n",i);
+            printf("#Removed the element at index %d \n",i);
             SET_DEFAULT_PRINT();
             return current_item;
         }
@@ -139,7 +138,7 @@ struct my_list_item* my_list_remove_item(struct my_list_item** head, unsigned in
     return NULL;
 }
 
-struct my_int_item* my_int_remove_item(struct my_int_item** head, unsigned int value)
+struct list_int_item* int_remove_item_at_value(struct list_int_item** head, unsigned int value)
 {
     if(!(*head)) return NULL;
 
@@ -148,12 +147,12 @@ struct my_int_item* my_int_remove_item(struct my_int_item** head, unsigned int v
         SET_RED_PRINT();
         printf("#Removed the head element \n");
         SET_DEFAULT_PRINT();
-        struct my_int_item* popped_item = TO_INT_LIST(my_list_pop((struct my_list_item**)(head)));
+        struct list_int_item* popped_item = TO_INT_LIST(list_pop((struct list_item**)(head)));
         return popped_item;
     }
 
-    struct my_int_item* current_item = TO_INT_LIST((*head))->list_item.next;
-    struct my_int_item* previous_item = (*head);
+    struct list_int_item* current_item = TO_INT_LIST((*head))->list_item.next;
+    struct list_int_item* previous_item = (*head);
 
     for (int i = 1; i < (*head)->list_item.count; i++)
     {
@@ -164,7 +163,7 @@ struct my_int_item* my_int_remove_item(struct my_int_item** head, unsigned int v
             current_item->list_item.next=NULL;
 
             SET_RED_PRINT();
-            printf("#Removed the %d element with value %d\n",i,current_item->value);
+            printf("#Removed the element at index %d with value %d\n",i,current_item->value);
             SET_DEFAULT_PRINT();
             return current_item;
         }
@@ -179,9 +178,9 @@ struct my_int_item* my_int_remove_item(struct my_int_item** head, unsigned int v
     return NULL;
 }
 
-void my_list_print(struct my_list_item* head)
+void list_print(struct list_item* head)
 {
-    struct my_list_item* current_item = head;
+    struct list_item* current_item = head;
 
     SET_GREEN_PRINT();
     printf("#The list length is = %d\n",head->count);
@@ -196,24 +195,24 @@ void my_list_print(struct my_list_item* head)
     }
 }
 
-struct my_list_item* my_list_reverse(struct my_list_item** head)
+struct list_item* list_reverse(struct list_item** head)
 {
     if(!(*head) || (*head)->count <2) return *head;
 
-    struct my_list_item* localHead = NULL;
+    struct list_item* localHead = NULL;
 
     const unsigned int count = (*head)->count;    
 
     for (int i = 0; i < count; i++)
     {
-        struct my_list_item* current_local_item = (*head);
+        struct list_item* current_local_item = (*head);
 
         for (int j = count-i-1; j > 0; j--)
         {
             current_local_item = current_local_item->next;
         }
 
-        my_list_append(&localHead,current_local_item);
+        list_append(&localHead,current_local_item);
     }
 
     SET_GREEN_PRINT();
@@ -224,39 +223,39 @@ struct my_list_item* my_list_reverse(struct my_list_item** head)
 
 int main(int argc, char** argv){
 
-    struct my_list_item* head = NULL; // it's a pointer not a my_list_item
+    struct list_item* head = NULL; // it's a pointer not a my_list_item
 
-    struct my_int_item first_int_item;
+    struct list_int_item first_int_item;
     first_int_item.value = 0;
-    my_list_append(&head,TO_GENERIC_LIST(&first_int_item.list_item));
+    list_append(&head,TO_GENERIC_LIST(&first_int_item.list_item));
 
-    struct my_int_item second_int_item;
+    struct list_int_item second_int_item;
     second_int_item.value = 2;
-    my_list_append(&head,MY_LIST(second_int_item));
+    list_append(&head,GET_MY_LIST(second_int_item));
 
-    struct my_int_item third_int_item;
+    struct list_int_item third_int_item;
     third_int_item.value = 4;
-    my_list_append(&head,MY_LIST(third_int_item));
+    list_append(&head,GET_MY_LIST(third_int_item));
 
-    struct my_int_item fourth_int_item;
+    struct list_int_item fourth_int_item;
     fourth_int_item.value = 8;
-    my_list_append(&head,MY_LIST(fourth_int_item));
+    list_append(&head,GET_MY_LIST(fourth_int_item));
 
-    struct my_int_item fifth_int_item;
+    struct list_int_item fifth_int_item;
     fifth_int_item.value = 16;
-    my_list_append(&head,MY_LIST(fifth_int_item));
+    list_append(&head,GET_MY_LIST(fifth_int_item));
 
-    my_list_print(head);
+    list_print(head);
 
-    my_list_remove_item(&head,1);
+    list_remove_item_at_index(&head,1);
 
-    my_list_print(head);
+    list_print(head);
 
-    my_int_remove_item((struct my_int_item**)&head,8);
+    int_remove_item_at_value((struct list_int_item**)&head,8);
 
-    my_list_print(head);
+    list_print(head);
 
-    struct my_int_item* reversed_list = TO_INT_LIST(my_list_reverse(&head));
+    struct list_int_item* reversed_list = TO_INT_LIST(list_reverse(&head));
 
-    my_list_print(TO_GENERIC_LIST(reversed_list));
+    list_print(TO_GENERIC_LIST(reversed_list));
 }
